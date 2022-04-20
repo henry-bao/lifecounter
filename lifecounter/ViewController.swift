e542ca028448eea4bfab87091152eb062267f1a1
henry-bao <henrybao@uw.edu>
2022-04-20 01:42:39 -0700

    Initial Commit

---
+++ lifecounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lifecounter
+//
+//  Created by Henry Bao on 4/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
